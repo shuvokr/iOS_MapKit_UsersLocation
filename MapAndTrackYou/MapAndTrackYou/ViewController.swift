467fa87e12067ddea7af0029ade066f7b852690a
shuvokr <shuvo@Shuvos-MacBook-Air.local>
2019-09-16 11:39:52 +0600

    Created a MapView IBOutlet to ViewController

--- MapAndTrackYou/MapAndTrackYou/ViewController.swift
+++ MapAndTrackYou/MapAndTrackYou/ViewController.swift
@@ -6,10 +6,13 @@
 //  Copyright © 2019 Shuvo. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
